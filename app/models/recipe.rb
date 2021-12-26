3563d5de6acb7a7fceb0c86e6547808b0a6baeea
keisuke.kuwahara <kuwa.90k@gmail.com>
2021-12-26 12:54:31 +0000

    fix favorite

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,14 +6,10 @@ class Recipe < ApplicationRecord
   
   mount_uploader :image, RacipeImageUploader
   
+  has_many :favorites, dependent: :destroy
   has_many :steps, dependent: :destroy
   has_many :ingredients, dependent: :destroy
   has_many :foods, through: :ingredients, source: :food
   accepts_nested_attributes_for :steps, reject_if: lambda {|attributes| attributes['explanation'].blank?}, allow_destroy: true
   accepts_nested_attributes_for :ingredients, reject_if: lambda {|attributes| attributes['quantity'].blank?}, allow_destroy: true
-  
-  # 栄養素を計算するメソッドを追加
-  # def carbo_sum(foods)
-    
-  # end
 end
