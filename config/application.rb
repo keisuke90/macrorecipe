d1b74966701c014296a324e200bebe9d064d466b
keisuke.kuwahara <kuwa.90k@gmail.com>
2021-12-31 00:07:32 +0000

    ja.yml

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,8 @@ module Macrorecipe
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.time_zone = 'Asia/Tokyo'
+    
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.yml").to_s]
   end
 end
