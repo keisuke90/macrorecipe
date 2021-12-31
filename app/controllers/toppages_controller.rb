0b0406571ca5f6f47d5d06211ea358513012d152
keisuke.kuwahara <kuwa.90k@gmail.com>
2021-12-31 02:04:55 +0000

    fix SyntaxError

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,18 +1,11 @@
 class ToppagesController < ApplicationController
-
-  # food_ids = []
-  # @recipes.each do |recipe|
-  #   recipe.ingredients.each do |ingredient|
-  #     food_ids << ingredient.food_id
-  #   end
-  # end
-  # foods = Food.where(id: food_ids.uniq)
   
   def index
     if params[:title].present?
-      @pagy, @recipes = pagy(Recipe.where('title LIKE ?', "%#{params[:title]}%"), items: 9)
+      @pagy, @recipes = pagy(Recipe.where('title LIKE ?', "%#{params[:title]}%").order(id: :desc), items: 9)
     else
       @pagy, @recipes = pagy(Recipe.all.order(id: :desc), items: 9)
     end
   end
+  
 end
