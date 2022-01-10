90ae52191caa421d20de68a190b16da16bd5ef24
keisuke.kuwahara <kuwa.90k@gmail.com>
2022-01-10 04:41:41 +0000

    fix select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-
 import $ from "jquery"
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import "channels"
@@ -13,24 +11,14 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 
-
-import "bootstrap"
-import "../src/application.scss"
-
 require("jquery")
 
 import 'select2'
 import 'select2/dist/css/select2.css'
-// document.addEventListener('turbolinks:load', () => {
-//   $('.js-select').select2({
-//     placeholder: 'Select an option',
-//     allowClear: true
-//   })
-// })
-$(function(){
+
+document.addEventListener('turbolinks:load', () => {
   $('.js-select').select2({
     placeholder: 'Select an option',
-    allowClear: true,
-    width: 'auto'
+    allowClear: true
   })
 })
