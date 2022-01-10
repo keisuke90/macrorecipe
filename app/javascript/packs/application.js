cee1866d2fde8dde983b8aac4fdfdb22b3b20767
keisuke.kuwahara <kuwa.90k@gmail.com>
2022-01-10 01:57:35 +0000

    select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,8 @@
 // that code so it'll be compiled.
 
 
+import $ from "jquery"
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import "channels"
@@ -11,7 +13,24 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 
+
+import "bootstrap"
+import "../src/application.scss"
+
 require("jquery")
 
 import 'select2'
 import 'select2/dist/css/select2.css'
+// document.addEventListener('turbolinks:load', () => {
+//   $('.js-select').select2({
+//     placeholder: 'Select an option',
+//     allowClear: true
+//   })
+// })
+$(function(){
+  $('.js-select').select2({
+    placeholder: 'Select an option',
+    allowClear: true,
+    width: 'auto'
+  })
+})
